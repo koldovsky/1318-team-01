2ea12d00bc15d206f8223d48baeddd3e267a264b
Vladyslav Dovhan <ljdufym13@gmail.com>
2024-12-24 15:23:15 +0000

    some updates in sweet treats js

--- js/sweet-treats-carousel.js
+++ js/sweet-treats-carousel.js
@@ -1,33 +1,33 @@
-let currentSlide = 0;
+const sweetTreatsContainer = document.querySelector(".sweet-treats__list");
+const sweetTreatsCards = document.querySelectorAll(".sweet-treats__card");
+const prevButton = document.querySelector(".sweet-treats__button-prev");
+const nextButton = document.querySelector(".sweet-treats__button-next");
+const cardWidth = sweetTreatsCards.length > 0 ? sweetTreatsCards[0].offsetWidth : 0;
+let currentIndex = 0;
 
 function updateCarousel() {
-  const carousel = document.getElementById("carousel");
-  const totalSlides = carousel.children.length;
-
-  const offset = -currentSlide * 100;
-  carousel.style.transform = `translateX(${offset}%)`;
-
-  document.querySelector(".sweet-treats__button-prev").disabled =
-    currentSlide === 0;
-  document.querySelector(".sweet-treats__button-next").disabled =
-    currentSlide === totalSlides - 1;
+  sweetTreatsContainer.style.transform = 'translateX(-' + (currentIndex * cardWidth) + 'px)';
 }
 
 function nextSlide() {
-  const carousel = document.getElementById("carousel");
-  if (currentSlide < carousel.children.length - 1) {
-    currentSlide++;
+  currentIndex = (currentIndex + 1) % sweetTreatsCards.length;
+  if (sweetTreatsContainer) {
     updateCarousel();
   }
 }
 
 function prevSlide() {
-  if (currentSlide > 0) {
-    currentSlide--;
-    updateCarousel();
-  }
+  currentIndex =
+    (currentIndex - 1 + sweetTreatsCards.length) % sweetTreatsCards.length;
+  updateCarousel();
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  updateCarousel();
-});
+if (nextButton) {
+  nextButton.addEventListener("click", nextSlide);
+}
+
+if (prevButton) {
+  prevButton.addEventListener("click", prevSlide);
+}
+
+updateCarousel();
