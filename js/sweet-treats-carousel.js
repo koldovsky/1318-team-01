7ad3a1ffc528c9662665fb34c4d515cf5b17419e
Vladyslav Dovhan <ljdufym13@gmail.com>
2024-12-29 21:45:44 +0200

    added carousel functionality

--- js/sweet-treats-carousel.js
+++ js/sweet-treats-carousel.js
@@ -1,33 +1,37 @@
-const sweetTreatsContainer = document.querySelector(".sweet-treats__list");
-const sweetTreatsCards = document.querySelectorAll(".sweet-treats__card");
+const list = document.querySelector(".sweet-treats__list");
 const prevButton = document.querySelector(".sweet-treats__button-prev");
 const nextButton = document.querySelector(".sweet-treats__button-next");
-const cardWidth = sweetTreatsCards.length > 0 ? sweetTreatsCards[0].offsetWidth : 0;
-let currentIndex = 0;
+const cards = document.querySelectorAll(".sweet-treats__card");
+let currentPosition = 0;
+let cardWidth;
+let gap;
 
-function updateCarousel() {
-  sweetTreatsContainer.style.transform = 'translateX(-' + (currentIndex * cardWidth) + 'px)';
+function updateCarouselValues() {
+  cardWidth = cards[0].offsetWidth;
+  gap = parseInt(window.getComputedStyle(list).gap);
 }
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % sweetTreatsCards.length;
-  if (sweetTreatsContainer) {
-    updateCarousel();
+nextButton.addEventListener("click", () => {
+  updateCarouselValues(); // Update values before scrolling
+  currentPosition++;
+  if (currentPosition >= cards.length) {
+    currentPosition = 0;
   }
-}
+  scrollToCard();
+});
 
-function prevSlide() {
-  currentIndex =
-    (currentIndex - 1 + sweetTreatsCards.length) % sweetTreatsCards.length;
-  updateCarousel();
-}
+prevButton.addEventListener("click", () => {
+  updateCarouselValues(); // Update values before scrolling
+  currentPosition--;
+  if (currentPosition < 0) {
+    currentPosition = cards.length - 1;
+  }
+  scrollToCard();
+});
 
-if (nextButton) {
-  nextButton.addEventListener("click", nextSlide);
+function scrollToCard() {
+  list.scrollTo({
+    left: (cardWidth + gap) * currentPosition,
+    behavior: "smooth",
+  });
 }
-
-if (prevButton) {
-  prevButton.addEventListener("click", prevSlide);
-}
-
-updateCarousel();
